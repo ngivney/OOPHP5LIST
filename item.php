1ca05d56065e98333c19ffa0d8702136862513c2
Nick Givney <ngivney@hotmail.com>
2015-03-25 19:48:16 -0700

    Create item.php
    Constructor Class

---
+++ item.php
@@ -0,0 +1,25 @@
+<?php
+class Item {
+	// Protect Item attributes
+  protected $id;
+	protected $client;
+	protected $venue;
+	// Constructor populates the attributes
+	public function __construct($id, $client, $venue)	{
+		$this->id = $id;
+		$this->person = $client;
+		$this->place = $venue;
+	}
+	// Returns the Primary Key
+	public function getId()	{
+		return $this->id;
+	}
+	// Returns Client Name
+	public function get_Person() {
+		return $this->person;
+	}
+	// Returns Client Venue
+	public function get_Venue() {
+		return $this->place;
+	}
+} // End of Item class
